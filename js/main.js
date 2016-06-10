f63eb4fdf4a8fb9f36e21f218fe27c3936c9ab3a
Surender Lohia <surender@tenmiles.com>
2016-06-10 10:48:35 +0530

    Basic setup and add new item with static content.

---
+++ js/main.js
@@ -0,0 +1,70 @@
+window.AppDispatcher = new Flux.Dispatcher();
+
+window.ListStore = {
+  items: [
+    {name: 'HTML', id: 1},
+    {name: 'CSS', id: 2},
+    {name: 'JavaScript', id: 3},
+  ]
+};
+
+MicroEvent.mixin(ListStore);
+
+var Todo = React.createClass({
+  componentDidMount: function() {
+    ListStore.bind('change', this.listChanged);
+  },
+
+  componentDidUnmount: function() {
+    ListStore.unbind('change', this.listChanged);
+  },
+
+  listChanged: function() {
+    this.forceUpdate();
+  },
+
+  createNewItem: function(evt) {
+    window.AppDispatcher.dispatch({
+      actionName: 'new-item',
+      newItem: {name: 'jQuery', id: 4}
+    })
+  },
+
+  render: function() {
+    var items = ListStore.items;
+
+    var htmlList = items.map(function(listItem) {
+      return <li id="listItem.id">
+          {listItem.name}
+        </li>
+    });
+
+    return <div>
+        <ul>
+          {htmlList}
+        </ul>
+        <button onClick={this.createNewItem}>Add New Item</button>
+      </div>
+  }
+});
+
+window.AppDispatcher.register(function(payload) {
+  switch(payload.actionName) {
+    case 'new-item':
+      ListStore.items.push(payload.newItem);
+      ListStore.trigger('change');
+      break;
+  }
+})
+
+ReactDOM.render(
+  <Todo/>,
+  document.getElementById('container')
+)
+
+
+
+
+
+
+
